90df7b8e1dbbabcd15b621cc4528a5c1e812ca1e
tjgustjr30723 <tjgustjr33@naver.com>
2021-10-04 19:48:00 +0900

    add hw2

---
+++ hw2.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void) 
+{
+	double km;
+	double result;
+	printf("Please enter kilometers: ");
+	scanf_s("%lf", &km);
+	result = km / 1.604;
+	printf("%.1lf is equal to %.1lf miles.", km, result);
+
+
+
+
+
+
+	return 0;
+}
